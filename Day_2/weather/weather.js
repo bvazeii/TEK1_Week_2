4614f483dbba1466dd81e538dd3c7eb1236fec9b
Bvazeii <bryan.vazeii@interapthq.com>
2016-09-23 00:44:30 -0400

    api's weather data from google and darksky was inputed one single card

--- Day_2/weather/weather.js
+++ Day_2/weather/weather.js
@@ -1,15 +1,32 @@
-$(function () {
-    $.ajax("https://api.darksky.net/forecast/ad5bfd5056d592066e1231dc4676c63d/37.529385,-82.518584", { dataType: "jsonp"})
-    // 57e2c0c91ebec60100061861 comes from postman , it does a search on codeacademy for this code id and grabs the likes
-        .done(function (data) {
-            var currentTemperature = Math.round(data.currently.temperature) + "&deg; F";
-
-//$(".currentTemperature").append('<h1>' + currentTemperature + '</h1>');
-      //$("#location").html(data.results[0].address_components[1].short_name);
-       $("#location2").html(data.longitude);
-        $("#currentCond").html(data.currently.summary);
-        $("#timeZone").html(data.timezone);
-        $("#hourly").html(data.hourly);
-
-        });
+   $(function(){
+ var google = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+ var darkSkyKey = "ad5bfd5056d592066e1231dc4676c63d/";
+  $("#submit").click(function(location){
+    var zip = $("#zip").val();
+    if (!zip) return;
+    console.log(zip);
+  $.ajax(google + zip + "&key=AIzaSyATyHyPFfnMzOjIy4CZ7ORUq2lbFlGVCYs" ).done(function(data){
+     var googleLat = data.results[0].geometry.location.lat;
+     var googleLng = data.results[0].geometry.location.lng;
+     var place = data.results[0].formatted_address;
+    //  console.log(googleLat);
+    //  console.log(googleLng);
+    //  console.log(data);
+   $.ajax("https://api.darksky.net/forecast/" + darkSkyKey + googleLat + "," + googleLng, { dataType: "jsonp"}).done(function(data) {
+    var temp = Math.round(data.currently.temperature);
+    var maxTemp = Math.round(data.daily.data[0].temperatureMax);
+    var minTemp = Math.round(data.daily.data[0].temperatureMin);
+    var summary = data.daily.data[0].summary;
+    var rain = data.daily.data[0].precipProbability;
+    
+    $("#location").append('<h1>' + place + '</h1>');
+     $("#currentTemperature").append('<h1>' + temp + "&deg; F"+'</h1>');
+     $("#temperatureMax").append("<h5>" + "Max Temperature: " + maxTemp + "&deg; F"+"</h5>"); 
+      $("#minTemperature").append("<h5>" + "Min Temperature: " + minTemp + "&deg; F"+"</h5>"); 
+       $("#rain").append("<h5>" + "Rain Chance: " + rain + "%" +"</h5>");     
+  })
+  }) 
+  })
 });
+
+
---
+++ Day_4/practice.js
@@ -0,0 +1,4 @@
+console.log("random: + math.random());
+var num= 49.88
+
+console.log(num);
